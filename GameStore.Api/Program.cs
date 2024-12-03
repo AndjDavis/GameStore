55ac4c8da364f72f73d14c6aaa94a4def5395e46
AndjDavis <andi.idavis@gmail.com>
2024-12-03 11:37:08 -0700

    automatically applying pending migrations on startup

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,10 +4,13 @@ using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
+
+// register gamestore context in the ef service container
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+app.MigrateDb();
 
 app.Run();
