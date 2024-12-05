284009d54a7690c91cb1064e0a5d84ddbd0104d4
AndjDavis <andi.idavis@gmail.com>
2024-12-05 11:42:30 -0700

    create Update game mapping, puts to /games/{id} now update db

--- GameStore.Api/Mapping/GameMapping.cs
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -16,6 +16,18 @@ public static class GameMapping
         };
     }
 
+    public static Game ToEntity(this UpdateGameDto game, int id)
+    {
+        return new Game()
+        {
+            Id = id,
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+        };
+    }
+
     public static GameDto ToDto(this Game game)
     {
         return new GameDto(game.Id, game.Name, game.Genre!.Name, game.Price, game.ReleaseDate);
